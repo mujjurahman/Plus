13596e79a5ffe6cd5a5234faca7778acf9da042b
Mujeeb Rahman <92824203+mujjurahman@users.noreply.github.com>
2023-06-17 19:40:47 +0530

    Update CustomEmailHandler.php

--- CustomEmailHandler.php
+++ CustomEmailHandler.php
@@ -1,22 +1,20 @@
-<?php
-
 namespace Drupal\pfe_med_connect\Plugin\WebformHandler;
 
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Mail\MailManagerInterface;
-use Drupal\Core\Session\AccountInterface;
 use Drupal\webform\Plugin\WebformHandlerBase;
 use Drupal\webform\WebformSubmissionInterface;
+use Drupal\Core\Mail\MailManagerInterface;
+use Drupal\Core\Render\RendererInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Custom webform submission handler.
+ * Form submission handler.
  *
  * @WebformHandler(
  *   id = "custom_webform_handler",
- *   label = @Translation("Custom handler"),
+ *   label = @Translation("Custom webform handler"),
  *   category = @Translation("Custom"),
- *   description = @Translation("Custom webform submission handler."),
+ *   description = @Translation("Handles the form submission."),
  *   cardinality = \Drupal\webform\Plugin\WebformHandlerInterface::CARDINALITY_SINGLE,
  *   results = \Drupal\webform\Plugin\WebformHandlerInterface::RESULTS_PROCESSED,
  * )
@@ -24,19 +22,35 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class CustomWebformHandler extends WebformHandlerBase {
 
   /**
-   * The mail manager.
+   * The mail manager service.
    *
    * @var \Drupal\Core\Mail\MailManagerInterface
    */
   protected $mailManager;
 
   /**
+   * The renderer service.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(MailManagerInterface $mail_manager, RendererInterface $renderer) {
+    $this->mailManager = $mail_manager;
+    $this->renderer = $renderer;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
-    $instance->mailManager = $container->get('plugin.manager.mail');
-    return $instance;
+    return new static(
+      $container->get('plugin.manager.mail'),
+      $container->get('renderer')
+    );
   }
 
   /**
@@ -44,43 +58,49 @@ class CustomWebformHandler extends WebformHandlerBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state, WebformSubmissionInterface $webform_submission) {
     $values = $webform_submission->getData();
-    $product = $values['nom_du_medicament_ou_produit'];  
-    $therapeutic_area = $values['aire_therapeutique'];  
-    $department = $values['departement']; 
 
-    if (empty($department)) {
-      // If no department, do not send an email.
-      return;
-    }
+    // Get the form values.
+    $product = $values['nom_du_medicament_ou_produit'];
+    $therapeutic_area = $values['aire_therapeutique'];
+    $department = $values['departement'];
 
-    // Retrieve email addresses from the custom table.
+    // Query the database.
     $query = \Drupal::database()->select('custom_table', 'ct');
-    $query->fields('ct', ['RMR_adresse_email', 'Backup_adresse_email']);
-    $query->condition('ct.Produit', $product);
-    $query->condition('ct.Aire_therapeutique', $therapeutic_area);
-    $query->condition('ct.Departement', $department);
-    $result = $query->execute()->fetchAssoc();
 
-    if ($result) {
-      $this->sendEmail('RMR_adresse_email', $result['RMR_adresse_email'], $values);
-      $this->sendEmail('Backup_adresse_email', $result['Backup_adresse_email'], $values);
+    if (!empty($product) && empty($therapeutic_area)) {
+      // Scenario 1: The user selects the product and keeps the therapeutics area field empty.
+      $query->condition('ct.Produit', $product);
     }
-  }
+    elseif (empty($product) && !empty($therapeutic_area)) {
+      // Scenario 2: The user selects the therapeutics area and keeps the Product field empty.
+      $query->condition('ct.Aire_therapeutique', $therapeutic_area);
+    }
+    else {
+      // Scenario 3: The user selects the product and therapeutics area.
+      $query->condition('ct.Produit', $product);
+    }
 
-  /**
-   * Sends an email.
-   *
-   * @param string $key
-   *   The email key.
-   * @param string $emailAddress
-   *   The email address.
-   * @param array $values
-   *   The form submission data.
-   */
-  private function sendEmail($key, $emailAddress, array $values) {
-    $langcode = \Drupal::currentUser()->getPreferredLangcode();
-    $params = ['values' => $values];
+    // Add the condition for the department, if it exists.
+    if (!empty($department)) {
+      $query->condition('ct.Department', $department);
+    }
 
-    $this->mailManager->mail('pfe_med_connect', $key, $emailAddress, $langcode, $params);
+    $query->fields('ct', ['RMR_adresse_email', 'Backup_adresse_email']);
+    $results = $query->execute()->fetchAll();
+
+    if (!empty($results)) {
+      // Prepare the mail parameters.
+      $params = [
+        'values' => $values,
+      ];
+
+      foreach ($results as $result) {
+        // Send a mail to the MSL email.
+        $this->mailManager->mail('pfe_med_connect', 'notification', $result->RMR_adresse_email, 'en', $params);
+
+        // Send a mail to the Backup email.
+        $this->mailManager->mail('pfe_med_connect', 'notification', $result->Backup_adresse_email, 'en', $params);
+      }
+    }
   }
 }
