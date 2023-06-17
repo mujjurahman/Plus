2e6372141223a70eae81fd9cb799dfca09180be5
Mujeeb Rahman <92824203+mujjurahman@users.noreply.github.com>
2023-06-17 15:53:20 +0530

    Create CustomEmailHandler.php

---
+++ CustomEmailHandler.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\pfe_med_connect\Plugin\WebformHandler;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Mail\MailManagerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\webform\Plugin\WebformHandlerBase;
+use Drupal\webform\WebformSubmissionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Form submission handler.
+ *
+ * @WebformHandler(
+ *   id = "custom_email_handler",
+ *   label = @Translation("Custom email handler"),
+ *   category = @Translation("Email"),
+ *   description = @Translation("Sends custom emails."),
+ *   cardinality = \Drupal\webform\Plugin\WebformHandlerInterface::CARDINALITY_SINGLE,
+ *   results = \Drupal\webform\Plugin\WebformHandlerInterface::RESULTS_PROCESSED,
+ *   submission = \Drupal\webform\Plugin\WebformHandlerInterface::SUBMISSION_REQUIRED,
+ * )
+ */
+class CustomEmailHandler extends WebformHandlerBase {
+
+  /**
+   * The mail manager.
+   *
+   * @var \Drupal\Core\Mail\MailManagerInterface
+   */
+  protected $mailManager;
+
+  /**
+   * Constructs the CustomEmailHandler.
+   *
+   * @param \Drupal\Core\Mail\MailManagerInterface $mail_manager
+   *   The mail manager.
+   */
+  public function __construct(MailManagerInterface $mail_manager) {
+    $this->mailManager = $mail_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $container->get('plugin.manager.mail')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state, WebformSubmissionInterface $webform_submission) {
+    // Get the submission values.
+    $values = $webform_submission->getData();
+
+    // Add your logic to query the custom_table for the email addresses here.
+    // If the required conditions are met, send the email.
+    // ...
+  }
+
+}
